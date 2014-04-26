2caac3c262a94b40d27866d39806add69c096ff3
DLDB <dpbergquist@gmail.com>
2014-04-26 17:16:55 +0100

    completed full functionality

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class Takeaway
 
   MENU = { :rat_kebab => 5,
@@ -10,16 +12,45 @@ class Takeaway
     MENU
   end
 
+  def submit(order)
+    validate(order)
+    delivery_time = calculate_delivery_time
+    sms = textmessage(delivery_time)
+    send!(sms)
+  end
+
+  def send!(sms)
+    account_sid = "AC6dc2accfba14a5404cdc7a64d7bbbb87"
+    auth_token = "3eb8d527bcfb9cb64abd7dac507e06a9"
+    client = Twilio::REST::Client.new account_sid, auth_token
+    from = "+15005550006" 
+    client.account.messages.create(
+    :from => from,
+    :to => "+447539008999",
+    :body => sms)
+  end
+
   def validate(order)
-    rejected_order = order.list.reject {|item| MENU.keys.include?(item)}
+    rejected_order = order.list.reject {|item| menu.keys.include?(item)}
     raise "Yuck. Fungus doesn't serve that." if !rejected_order.empty?
     raise "Wrong price!" if calculate_cost(order) != order.expected_price
     true
   end
 
   def calculate_cost(order)
-    costs = order.list.map {|item| MENU[item] }
+    costs = order.list.map {|item| menu[item] }
     costs.inject(&:+)
+  end
+
+  def calculate_delivery_time
+    time = [Time.now.hour + 1, Time.now.min]
+    "#{time[0]}.#{time[1]}"
+  end
+
+  
+
+  def textmessage(time)
+    "Fungus T. Bogeyman tanks you for order. Will be delivered before #{time}"
   end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -7,36 +7,66 @@ describe Takeaway do
   let(:bad_item_order) {double :order, list: [:CHEEZBURGER, :tarantula_fries]}
   let(:bad_maths_order) {double :order, list: [:bogey_burger, :strong_french_gallwasp], expected_price: 12 }
 
-  it 'can return a price for a rat kebab' do
-    expect(bogeybar.menu[:rat_kebab]).to eq 5
+  context 'checking the menu' do
+
+    it 'can return a price for a rat kebab' do
+      expect(bogeybar.menu[:rat_kebab]).to eq 5
+    end
   end
 
-  it 'can check if every item on a valid order is valid' do
-    expect(good_order).to receive(:expected_price)
-    expect(bogeybar.validate(good_order)).to be_true
+  context 'when validating an order,' do
+
+    it 'can check if every item on a valid order is valid' do
+      expect(good_order).to receive(:expected_price)
+      expect(bogeybar.validate(good_order)).to be_true
+    end
+
+    it 'raises an error if a requested item is not for sale' do
+      expect{bogeybar.validate(bad_item_order)}.to raise_error "Yuck. Fungus doesn't serve that."
+    end
+
+    it 'can calculate and verify the total price of the order' do
+      expect(bogeybar.calculate_cost(good_order)).to eq 15
+    end
+
+    it 'can raise an error for an incorrect price' do
+      expect(bad_maths_order).to receive(:expected_price)
+      expect{bogeybar.validate(bad_maths_order)}.to raise_error "Wrong price!"
+    end
   end
 
-  it 'raises an error if a requested item is not for sale' do
-    expect{bogeybar.validate(bad_item_order)}.to raise_error "Yuck. Fungus doesn't serve that."
+  context 'when submitting a valid order,' do
+    before :each do 
+      bogeybar.stub(:send!).and_return(true)
+      bogeybar.stub(:calculate_delivery_time).and_return("18.30")
+      expect(good_order).to receive(:expected_price)
+    end
+
+    it 'the order will be processed' do
+      expect(bogeybar.submit(good_order)).to be_true
+    end
+
+    it 'calculates delivery time' do
+      expect(bogeybar).to receive(:calculate_delivery_time)
+      bogeybar.submit(good_order)
+    end
+
+    it 'will send a text' do
+      expect(bogeybar).to receive(:send!)
+      bogeybar.submit(good_order)
+    end
   end
 
-  it 'can calculate and verify the total price of the order' do
-    expect(bogeybar.calculate_cost(good_order)).to eq 15
-  end
+    context 'when submitting a non-valid order' do
 
-  it 'can raise an error for an incorrect price' do
-    expect(bad_maths_order).to receive(:expected_price)
-    expect{bogeybar.validate(bad_maths_order)}.to raise_error "Wrong price!"
-  end
+      it 'will not calculate a delivery time' do
+        expect{bogeybar.submit(bad_item_order)}.to raise_error
+        expect(bogeybar).not_to receive(:calculate_delivery_time)
+      end
 
-  it 'can submit a valid order' do
-  end
-
-  it 'can calculate a delivery time' do
-  end
-
-  it 'can send a text' do
-  end
-
-
+      it 'will not send a text' do
+      expect{bogeybar.submit(bad_item_order)}.to raise_error
+      expect(bogeybar).not_to receive(:send!)
+      end
+    end
 end
