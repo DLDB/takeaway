d8f39af340f92836b8efd5c4756cf832d9ee809f
DLDB <dpbergquist@gmail.com>
2014-04-26 13:13:39 +0100

    completed order

--- lib/order.rb
+++ lib/order.rb
@@ -7,8 +7,23 @@ class Order
     :catseye_bubble_tea => 1,
     :I_CAN_HAZ_CHEEZBURGER => 11 }
 
-  def list
-    2
+  attr_reader :list, :expected_price
+
+  def initialize
+    @list = generate_list
+    @expected_price = calculate_cost
+  end 
+
+  def order_randomizer
+    POSSIBLE_WISHES.keys.sample
+  end 
+
+  def generate_list
+    [order_randomizer, order_randomizer]
+  end
+
+  def calculate_cost
+    POSSIBLE_WISHES[@list[0]] + POSSIBLE_WISHES[@list[1]]
   end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,18 @@
 require 'order'
 
 describe Order do
+
+  let(:order) {Order.new}
+  before :each do
+    order.stub(:generate_list).and_return([:bogey_burger, :tarantula_fries])
+  end
   
   it 'generates order with list of 2 customer wishes' do
     expect(order.list.count).to eq 2
   end
 
-  xit 'calculates expected price' do
+  it 'calculates expected price' do
+    expect(order.expected_price).to eq 15
   end
 
 
